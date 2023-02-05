b6e664f983b203ab5f2d651590d2607f70b0c375
Ritika Malik <ritikamalik100102@gmail.com>
2023-02-06 01:35:33 +0530

    styling

--- JavaScript.js
+++ JavaScript.js
@@ -1,11 +1,11 @@
 function myFunc(){
     alert("You Clicked me");
  }
- var inputval = document.querySelector('#inputcity')
+var inputval = document.querySelector('#inputcity')
 var btn = document.querySelector('#search');
 var city = document.querySelector('#cityoutput')
 var descrip = document.querySelector('#description')
-var temp = document.querySelector('#temp')
+var temp = document.querySelector('#temperature')
 var wind = document.querySelector('#wind')
 apik = "cd8c5b68ee7287f24dfd9a279a59629f"
 function convertion(val){
@@ -16,13 +16,13 @@ btn.addEventListener('click',function(){
 .then(res => res.json())
 .then(data => {
     var nameval = data['name']
-    var descrip = data['weather']['0']['description']
+    var descript = data['weather']['0']['description']
     var tempature = data['main']['temp']
     var wndspd = data['wind']['speed']
 
-    cityoutput.innerHTML=`Weather of <span>${nameval}<span>`
+    city.innerHTML=`Weather of <span>${nameval}<span>`
     temp.innerHTML = `Temperature: <span>${ convertion(tempature)} C</span>`
-    description.innerHTML = `Sky Conditions: <span>${descrip}<span>`
+    descrip.innerHTML = `Sky Conditions: <span>${descript}<span>`
     wind.innerHTML = `Wind Speed: <span>${wndspd} km/h<span>`
 })
 .catch(err => alert('You entered Wrong city name'))
